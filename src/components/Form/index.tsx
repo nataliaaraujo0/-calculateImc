da1c3c450e82fafcd56b861f739b35329f9b72d4
Natalia <natalia.araujo@yazo.com.br>
2022-09-23 01:02:30 -0300

    chore: cadastrando dados no formulario

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,11 +1,45 @@
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { FormWrapper } from "./styled";
-export const Form = () => {
-  return (
-    <FormWrapper>
-      <input type="text" placeholder="Qual é seu peso?" />
-      <input type="text" placeholder="Qual é  a sua altura?" />
 
-      <button>Enviar</button>
+const newImcFormSchema = z.object({
+  weight: z.number(),
+  height: z.number(),
+  name: z.string(),
+});
+
+type NewImcFormInputs = z.infer<typeof newImcFormSchema>;
+
+export function Form() {
+  // IMC = Peso ÷ (Altura × Altura)
+
+  const { register, handleSubmit } = useForm<NewImcFormInputs>({
+    resolver: zodResolver(newImcFormSchema),
+  });
+
+  async function handleCreateNewImc(data: NewImcFormInputs) {
+    console.log(data);
+  }
+  return (
+    <FormWrapper onSubmit={handleSubmit(handleCreateNewImc)}>
+      <input
+        type="text"
+        placeholder="Qual é o seu nome?"
+        {...register("name")}
+      />
+      <input
+        type="number"
+        placeholder="Qual é seu peso?"
+        {...register("weight", { valueAsNumber: true })}
+      />
+      <input
+        type="number"
+        placeholder="Qual é  a sua altura?"
+        {...register("height", { valueAsNumber: true })}
+      />
+
+      <button type="submit">Enviar</button>
     </FormWrapper>
   );
-};
+}
--- src/components/Form/styled.ts
+++ src/components/Form/styled.ts
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const FormWrapper = styled.div`
+export const FormWrapper = styled.form`
 display: flex;
 flex-direction: column;
 width: 282px;
