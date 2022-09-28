f0b2cabeff0c5cc77c2e5a113ce10504f422e4ec
Deividy Ferreira <fnsdeividy@gmail.com>
2022-09-27 21:35:13 -0300

    update index

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -17,65 +17,78 @@ const newImcFormSchema = z.object({
 type NewImcFormInputs = z.infer<typeof newImcFormSchema>;
 
 export function Form({ formatedImc }: FormProps) {
-  //const {
-  //sobrepeso,
-  //setResultImc,
-  //resultImc,
-  //pesoNormal,
-  //obsediadeC,
-  //obsediadeB,
-  //obsediadeA,
-  //abaixoDoPeso,
-  //} = useCalcImc();
+ 
 
-  const [resultImc, setResultImc] = useState(0);
+
+  const [scale, setScale] = useState('');
+  const [color, setColor] = useState('')
+
 
   const { register, handleSubmit } = useForm<NewImcFormInputs>({
     resolver: zodResolver(newImcFormSchema),
   });
 
   async function handleCalImc(data: NewImcFormInputs) {
+
     const { weight, height } = data;
     const resultImc = weight / (height * height);
     const formatedImc = resultImc.toFixed(2);
-    setResultImc(+formatedImc);
-    console.log(formatedImc);
+    if(Number(formatedImc) < 18.5) {
+      setColor('red')
+      return setScale('Abaixo do Peso')
+
+    }else if(Number(formatedImc) >= 18.5 && Number(formatedImc) < 24.9) {
+
+      setColor('green')
+      return setScale('Peso Ideal')
+
+    } else if(Number(formatedImc) >= 24.9 && Number(formatedImc) < 34.9) {
+
+      setColor('yellow')
+      return setScale('Obesidade A')
+
+    }else if(Number(formatedImc) >= 34.9 && Number(formatedImc) < 39.9) {
+
+      setColor('orange')
+      return setScale('Obesidade B')
+
+    }else if (Number(formatedImc) >= 39.9) {
+
+      setColor('red')
+      return setScale('Obesidade C')
+
+    }
+   
   }
 
-  const abaixoDoPeso = resultImc < 18.5;
-  const pesoNormal = resultImc >= 18.5;
-  const sobrepeso = resultImc >= 24.9;
-  const obsediadeA = resultImc >= 29.9;
-  const obsediadeB = resultImc >= 34.9;
-  const obsediadeC = resultImc >= 39.9;
-
   return (
+    <>
     <FormWrapper onSubmit={handleSubmit(handleCalImc)}>
       <input
         type="text"
-        placeholder="Qual é o seu nome?"
+        placeholder="Digite seu nome"
         {...register("name")}
       />
       <input
         type="tex"
-        placeholder="Qual é seu peso?"
+        placeholder="Digite seu peso (kg)"
         {...register("weight", { valueAsNumber: true })}
       />
       <input
         type="text"
-        placeholder="Qual é  a sua altura?"
+        placeholder="Digite sua altura (m)"
         {...register("height", { valueAsNumber: true })}
       />
 
-      <button type="submit">Enviar</button>
-
-      {resultImc}
-      {abaixoDoPeso ? <h1>abaixoDoPeso</h1> : "null"}
-      {pesoNormal && <h1>pesoNormal</h1>}
-      {sobrepeso && <h1>sobrepeso</h1>}
-      {obsediadeA && <h1>obsediadeA</h1>}
-      {obsediadeB && <h1>obsediadeB</h1>}
-      {obsediadeC && <h1>obsediadeC</h1>}
+      <button onSubmit={handleSubmit(handleCalImc)} type="submit">Enviar</button>
+      
     </FormWrapper>
+    {scale ? (
+        <h1 style={{ color: color}}>
+          {scale}
+        </h1>
+      ): ''}
+    </>
+    
   );
 }
